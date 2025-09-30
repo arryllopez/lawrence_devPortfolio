7237920e425307c5ffe65a405745e2784699eb94
arryllopez <arryllopez7@gmail.com>
2025-09-30 00:14:39 -0400

    changing import of ReactDOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 //starting point of the react website
-import ReactDOM from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />); //react is going to hook into a single div on our html file with the id of root and render our app component inside of it
