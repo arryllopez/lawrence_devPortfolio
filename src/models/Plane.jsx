536190d261d18b8887e656b97cc79a6604849e89
arryllopez <arryllopez7@gmail.com>
2025-10-13 15:45:48 -0400

    plane.jsx

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -3,7 +3,7 @@ import { useGLTF } from '@react-three/drei'
 import planeScene from '../assets/3d/plane.glb'
 
 const Plane = () => {
-  const {scene, animations} = useGLTF(planeScene)
+  const {scene, animations} = useGLTF(planeScene);
 
   return (
     <mesh>
