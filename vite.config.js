80cd0e3d1e9d1bb937917c67aec20848a9ae5076
arryllopez <arryllopez7@gmail.com>
2025-11-11 18:52:18 -0500

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude : ['**/*.gltf', '**/*.glb', '**/*.hdr' ]
+  assetsInclude : ['**/*.gltf', '**/*.glb', '**/*.hdr' ],
+  base : '/lawrence_devPortfolio/',
 })
